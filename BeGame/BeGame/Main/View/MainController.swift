439f6a93e3b6c0e6a2c500fd425375dd0698702f
aleksejrevakin <alexey.amahasla@gmail.com>
2023-05-23 20:56:28 +0300

    Create MainControler and MainViewModel

---
+++ BeGame/BeGame/Main/View/MainController.swift
@@ -0,0 +1,19 @@
+//
+//  MainController.swift
+//  BeGame
+//
+//  Created by Алексей Ревякин on 23.05.2023.
+//
+
+import UIKit
+
+class MainController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .white
+        navigationItem.title = "Main"
+    }
+
+}
