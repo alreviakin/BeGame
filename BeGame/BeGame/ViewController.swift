dda15138302089ad0dd1b2a196f33e8a8ab09b85
aleksejrevakin <alexey.amahasla@gmail.com>
2023-05-23 19:20:50 +0300

    Create project

---
+++ BeGame/BeGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BeGame
+//
+//  Created by Алексей Ревякин on 23.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
