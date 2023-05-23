9687f57ff3aac5f17f9030a50bf49169ba842a0d
aleksejrevakin <alexey.amahasla@gmail.com>
2023-05-23 20:58:46 +0300

    Create TabBarController

---
+++ BeGame/BeGame/Navigation/TabBarController.swift
@@ -0,0 +1,63 @@
+//
+//  TabBarController.swift
+//  BeGame
+//
+//  Created by Алексей Ревякин on 23.05.2023.
+//
+
+import UIKit
+
+enum Tabs: Int, CaseIterable {
+    case main
+    case play
+}
+
+class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configure()
+    }
+    
+    private func configure() {
+        tabBar.backgroundColor = .white
+        
+        let controllers: [UINavigationController] = Tabs.allCases.map { tab in
+            let controller = UINavigationController(rootViewController: getController(tab: tab))
+            controller.tabBarItem = UITabBarItem(
+                title: getBarTitle(tab: tab),
+                image: getBarImage(tab: tab),
+                tag: tab.rawValue)
+            return controller
+        }
+        setViewControllers(controllers, animated: true)
+    }
+    
+    private func getController(tab: Tabs) -> UIViewController {
+        switch tab {
+        case .main:
+            return MainController()
+        case .play:
+            return PlayController()
+        }
+    }
+    
+    private func getBarTitle(tab: Tabs) -> String {
+        switch tab {
+        case .main:
+            return "Главный"
+        case .play:
+            return "Начать игру"
+        }
+    }
+    
+    private func getBarImage(tab: Tabs) -> UIImage {
+        switch tab {
+        case .main:
+            return R.TabBar.Image.main
+        case .play:
+            return R.TabBar.Image.play
+        }
+    }
+
+}
---
+++ BeGame/BeGame/Resources/Resources.swift
@@ -0,0 +1,22 @@
+//
+//  Resources.swift
+//  BeGame
+//
+//  Created by Алексей Ревякин on 23.05.2023.
+//
+
+import Foundation
+import UIKit
+
+enum R {
+    enum color {
+        
+    }
+    
+    enum TabBar {
+        enum Image {
+            static let main = UIImage(systemName: "house")!
+            static let play = UIImage(systemName: "play")!
+        }
+    }
+}
