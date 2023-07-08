e70332364538ea3fcd66ed0282a184eb9f3a502d
aleksejrevakin <alexey.amahasla@gmail.com>
2023-07-08 14:44:49 +0300

    Create func for manager data

--- BeGame/Managers/DateManager.swift
+++ BeGame/Managers/DateManager.swift
@@ -25,9 +25,33 @@ class DateManager {
         return dateFormater.date(from: dateString) ?? Date()
     }
     
+    func dateToString(date: Date) -> String {
+        let dateFormater = DateFormatter()
+        dateFormater.dateFormat = "dd.MM.yy"
+        return dateFormater.string(from: date)
+    }
+    
     func getCurrentDate() -> Date {
         let dateFormater = DateFormatter()
         dateFormater.dateFormat = "dd.MM.yy"
         return stringToDate(dateString: getCurrentDate())
+    }
+    
+    func intToTimeString(countTime: Int) -> String {
+        let hours = countTime / 3600
+        let minutes = countTime / 60 % 60
+        let seconds = countTime % 60
+        
+        var times: [String] = []
+        if hours > 0 {
+          times.append("\(hours)h")
+        }
+        if minutes > 0 {
+          times.append("\(minutes)m")
+        }
+        if hours == 0 {
+            times.append("\(seconds)s")
+        }
+        return times.joined(separator: " ")
     }
 }
